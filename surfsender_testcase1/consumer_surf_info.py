b90ed81caf161007560482d04026638b561eafc5
Mike Kramer <mkramer265@gmail.com>
2020-02-23 21:06:32 -0500

    cleaning up some code in the consumer files.

--- surfsender_testcase1/consumer_surf_info.py
+++ surfsender_testcase1/consumer_surf_info.py
@@ -8,26 +8,34 @@ from kafka import KafkaConsumer
 # Once executed, this script will take the messages living in the topic & send them to an Aiven PostgreSQL database.
 
 if __name__ == '__main__':
-	parsed_topic_name = 'surf_data_topic'
+	parsed_topic_name = 'surf_data_topic5555'
 
 	consumer = KafkaConsumer(parsed_topic_name, auto_offset_reset='earliest',
 							 bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
 	for msg in consumer:
-		message = msg.value.decode("utf-8")
+		message_raw = msg.value.decode("utf-8")
+		
+		if 'flat' in message_raw:
+			message = message_raw.replace("ft", "")
 
-		id_ = '5'
+			conn = psycopg2.connect("dbname=surf_db user=admin3 password=admin323 host=localhost")
+			cur = conn.cursor()
+			# cur.execute("INSERT INTO surf_data (id, surf_info) VALUES (?,?)", (id_,message))
+			cur.execute("INSERT INTO surf_conditions (wave_info) VALUES (%s)", (message,))
+			conn.commit()
+			cur.close()
+			conn.close()
 
+		else:
+			conn = psycopg2.connect("dbname=surf_db user=admin3 password=admin323 host=localhost")
+			cur = conn.cursor()
+			# cur.execute("INSERT INTO surf_data (id, surf_info) VALUES (?,?)", (id_,message))
+			cur.execute("INSERT INTO surf_conditions (wave_info) VALUES (%s)", (message_raw,))
+			conn.commit()
+			cur.close()
+			conn.close()
 
-		conn = psycopg2.connect("dbname=surf_db user=admin3 password=admin323 host=localhost")
-		cur = conn.cursor()
-		# cur.execute("INSERT INTO surf_data (id, surf_info) VALUES (?,?)", (id_,message))
-		cur.execute("INSERT INTO surf_conditions (wave_info) VALUES (%s)", (message,))
-		conn.commit()
-		cur.close()
-		conn.close()
-
-
-		print(message)
+			print(message_raw)
 
 	if consumer is not None:
 		consumer.close()
