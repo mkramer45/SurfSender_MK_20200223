c0f8bcc457164cc28642fa8d346537777f7df20c
Mike Kramer <mkramer265@gmail.com>
2020-02-23 21:03:34 -0500

    small change made to if/else logic in the consumer.

--- surfsender_testcase2/2_consumer_surf_info.py
+++ surfsender_testcase2/2_consumer_surf_info.py
@@ -8,7 +8,7 @@ from kafka import KafkaConsumer
 # Once executed, this script will take the messages living in the topic & send them to an Aiven PostgreSQL database.
 
 if __name__ == '__main__':
-	parsed_topic_name = 'surf_data_topic555'
+	parsed_topic_name = 'surf_data_topic5555'
 
 	consumer = KafkaConsumer(parsed_topic_name, auto_offset_reset='earliest',
 							 bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
@@ -18,7 +18,6 @@ if __name__ == '__main__':
 		if 'flat' in message_raw:
 			message = message_raw.replace("ft", "")
 
-
 			conn = psycopg2.connect("dbname=surf_db user=admin3 password=admin323 host=localhost")
 			cur = conn.cursor()
 			# cur.execute("INSERT INTO surf_data (id, surf_info) VALUES (?,?)", (id_,message))
@@ -27,8 +26,16 @@ if __name__ == '__main__':
 			cur.close()
 			conn.close()
 
+		else:
+			conn = psycopg2.connect("dbname=surf_db user=admin3 password=admin323 host=localhost")
+			cur = conn.cursor()
+			# cur.execute("INSERT INTO surf_data (id, surf_info) VALUES (?,?)", (id_,message))
+			cur.execute("INSERT INTO surf_conditions (wave_info) VALUES (%s)", (message_raw,))
+			conn.commit()
+			cur.close()
+			conn.close()
 
-			print(message)
+			print(message_raw)
 
 	if consumer is not None:
 		consumer.close()
